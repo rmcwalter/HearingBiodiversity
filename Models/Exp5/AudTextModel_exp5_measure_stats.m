81a8efd8102dd6cd9a9446668b0ad538daa027c3
Richard McWalter <mcwalter@mit.edu>
2026-06-26 13:32:08 -0400

    Fix file path for WAV file collection in observer model

--- Models/Exp5/AudTextModel_exp5_measure_stats.m
+++ Models/Exp5/AudTextModel_exp5_measure_stats.m
@@ -35,7 +35,7 @@ load(['../Model_base/_system/AudSys_Setup_' mfb_mode '.mat'])
 
 % Collect all WAV files for 50 subjects
 for k = 1:50
-    dt{k} = dir(['/Volumes/SD512/macaulay_orig/McWalter ML Request/exp8/_discrimination_task_8a_S' num2str(k,'%02.f') '/*.wav']);
+    dt{k} = dir(['../exp8/_discrimination_task_8a_S' num2str(k,'%02.f') '/*.wav']);
 end
 
 % Concatenate into a single flat list d
